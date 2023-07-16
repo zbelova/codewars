fe7de6b6d79a22124c8431d2e1d737bfcc2f273e
Zoya Belova (macbook) <alirasirin@gmail.com>
2023-07-16 12:55:08 +0300

    New katas

--- bin/katas.dart
+++ bin/katas.dart
@@ -1,6 +1,4 @@
-void runKatas() {
-
-}
+void runKatas() {}
 
 //TODO
 //Array Leaders (Array Series #3)
@@ -11,8 +9,8 @@ void runKatas() {
 List<int> arrayLeaders(numbers) {
   List<int> result = [];
   for (int i = 0; i < numbers.length; i++) {
-    int reduced = i == numbers.length - 1?0:List.from(numbers.getRange(i+1, numbers.length)).reduce((v, e) => v+e);
-    if (numbers[i] > reduced)  {
+    int reduced = i == numbers.length - 1 ? 0 : List.from(numbers.getRange(i + 1, numbers.length)).reduce((v, e) => v + e);
+    if (numbers[i] > reduced) {
       result.add(numbers[i]);
     }
   }
@@ -38,8 +36,7 @@ List<int> arrayLeaders2(List<int> numbers) {
 // stairs = [sunday,monday,tuesday,wednesday,thursday,friday,saturday]
 // sunday = [6737, 7244, 5776, 9826, 7057, 9247, 5842, 5484, 6543, 5153, 6832, 8274, 7148, 6152, 5940, 8040, 9174, 7555, 7682, 5252, 8793, 8837, 7320, 8478, 6063, 5751, 9716, 5085, 7315, 7859, 6628, 5425, 6331, 7097, 6249, 8381, 5936, 8496, 6934, 8347, 7036, 6421, 6510, 5821, 8602, 5312, 7836, 8032, 9871, 5990, 6309, 7825]
 
-int stairsIn20(List<List<int>> arr) =>
-    arr.expand((p) => p).toList().map((e) => e).toList().reduce((a, b) => a + b)*20;
+int stairsIn20(List<List<int>> arr) => arr.expand((p) => p).toList().map((e) => e).toList().reduce((a, b) => a + b) * 20;
 
 //TODO
 // Vowel Count
@@ -47,17 +44,22 @@ int stairsIn20(List<List<int>> arr) =>
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 // The input string will only consist of lower case letters and/or spaces.
 
-int getCount(String inputStr){
+int getCount(String inputStr) {
   List listAll = inputStr.split('');
   var cleared = 0;
   for (var item in listAll) {
-    if (['a', 'e', 'i', 'o', 'u'].contains(item)) cleared ++;
+    if (['a', 'e', 'i', 'o', 'u'].contains(item)) cleared++;
   }
   return cleared;
 }
 
 int getCount2(String str) => new RegExp('[aeiou]').allMatches(str).length;
 
-int getCount3(String inputStr){
-  return inputStr.split('').fold(0, (a, b) => a += 'aeiou'.contains(b) ? 1 : 0 );
+int getCount3(String inputStr) {
+  return inputStr.split('').fold(0, (a, b) => a += 'aeiou'.contains(b) ? 1 : 0);
 }
+
+//TODO
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+bool XO(str) => 'x'.allMatches(str.toLowerCase()).length == 'o'.allMatches(str.toLowerCase()).length;
